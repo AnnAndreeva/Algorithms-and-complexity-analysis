2720eeb4522aee66cf68fce54179f5d3e18683a5
Anna Andreeva <annandreeva21@gmail.com>
2022-08-28 18:06:29 +0400

    LR 03

---
+++ Alg_03/Alg_03/LinkList.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alg_03
+{
+    //элемент списка
+    public class Node
+    {
+        public int info;
+        public Node Next { get; set; }
+        public Node Random { get; set; }
+        public Node()
+        { }
+        public Node(int info)
+        {
+            this.info = info;
+        }
+    }
+    class LinkList
+    {
+
+        public Node head;
+        public int count;
+        public LinkList()
+        {
+            head = null;
+        }
+        //вывод списка
+        public override string ToString()
+        {
+            if (head == null)
+                return "Список пуст!";
+            Node curr = head;
+            string str = head.info.ToString();
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                str = str + ' ' + curr.info.ToString();
+            }
+            return str;
+        }
+        //добаляем элемент списка
+        public void Add(int a)
+        {
+            if (head != null)
+            {
+                Node p = head;
+                while (p.Next != null)
+                {
+                    p = p.Next;
+                }
+                p.Next = new Node(a);
+            }
+            else
+            {
+                head = new Node(a);
+            }
+            count++;
+            Rand();
+        }
+        public void Rand()//добавляем рандомную ссылку для каждого элемента
+        {
+            Random rand = new Random();
+            Node q = head;
+            while (q != null)
+            {
+                Node p = head;
+                int k = 1;
+                int search = rand.Next(1, count);
+                while (k != search)
+                {
+                    p = p.Next;
+                    k++;
+                }
+                q.Random = p;
+                q = q.Next;
+            }
+        }
+
+        //Задание 1. Дан однонаправленный список с петлей. Его последний элемент содержит указатель на один из элементов этого списка.
+        //Найдите начальный узел петли
+        public Node FindBegining(Node head)//поиск начала петли, сложность O(n)
+        {
+            Node slow = head;
+            Node fast = head;
+
+            //Находим первую точку встречи LOOP_SIZE-k шагов по связному списку.
+            while (fast != null && fast.Next != null)
+            {
+                slow = slow.Next;//сдвигаемм на 1 шаг
+                fast = fast.Next.Next;//сдвигаемм на 2 шага
+                if (slow == fast)//столкновение
+                    break;
+            }
+
+            // Ошибка - нет точки встречи, следовательно, нет петли 
+            if (fast == null || fast.Next == null)
+                return null;
+
+            //Перемещаем медленный бегунок в начало списка (Head). Быстрый остается в точке встречи.
+			//Если указатели продолжат движение с той же скоростью, то встретятся в точке Loop Start
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow.Next;
+                fast = fast.Next;
+            }
+
+            // Возвращаем точку начала петли. 
+            return fast;
+        }
+        //Задание 3. Удалите дубликаты из несортированного связного списка. Память константна
+        public void deleteDups1(Node head)//удаление дубликатов, сложность O(n^2)
+        {
+            if (head == null) return; //если список пуст
+
+            //используем 2 элемента для проверки
+            Node current = head;//значение текущего элемента, работает через связный список
+            while (current != null)
+            {
+                // Удаляем все следующие элементы с таким же значением 
+                Node runner = current;//проверяет все последующие узлы на наличие дубликатов
+                while (runner.Next != null)
+                {
+                    if (runner.Next.info == current.info)
+                    {
+                        runner.Next = runner.Next.Next;
+                    }
+                    else
+                    {
+                        runner = runner.Next;
+                    }
+                }
+                current = current.Next;
+            }
+        }
+        public void deleteDups2(Node head)//удаление дубликатов, сложность O(n)
+        {
+            Hashtable table = new Hashtable();
+            Node previous = null;
+           // выполняется проход по списку, каждый элемент которого добавляется в хэш - таблицу
+            while (head != null)
+            {
+               // Когда обнаруживается повторяющийся элемент, он удаляется, и цикл продолжает работу
+                if (table.ContainsKey(head.info))
+                {
+                    previous.Next = head.Next;
+                }
+                else
+                {
+                    table.Add(head.info, true);
+                    previous = head;
+                }
+                head = head.Next;
+            }
+        }
+
+        //Задание 4. Разбейте связный список вокруг некоторого значения так, чтобы все меньше узлы оказались перед узлами,
+        //большими или равными этому значению
+        public Node partition(Node node, int x) // сложность O(n)
+        {
+            Node beforeStart = null;
+            Node beforeEnd = null;
+
+            Node afterStart = null;
+            Node afterEnd = null;
+
+            // Разбиваем список 
+            while (node != null)
+            {
+                Node next = node.Next;
+                node.Next = null;
+                if (node.info < x)
+                {
+                    // Вставляем узел в конец списка before
+                    if (beforeStart == null)
+                    {
+                        beforeStart = node;
+                        beforeEnd = beforeStart;
+                    }
+                    else
+                    {
+                        beforeEnd.Next = node;
+                        beforeEnd = node;
+                    }
+                }
+                else
+                {
+                    // Вставляем узел в конец списка after
+                    if (afterStart == null)
+                    {
+                        afterStart = node;
+                        afterEnd = afterStart;
+                    }
+                    else
+                    {
+                        afterEnd.Next = node;
+                        afterEnd = node;
+                    }
+                }
+                node = next;
+            }
+
+            if (beforeStart == null)
+            {
+                return afterStart;
+            }
+
+            // Слияние списков before и after 
+            beforeEnd.Next  = afterStart;
+            return beforeStart;
+        }
+        //Задание 5. Найдите в односвязном списке к-ый с конца элемент. Есть только операция получения следующего элемента
+        //и указатель на первый элемент. Алгоритм должен быть оптимален по времени и памяти
+        public int findK(Node head, int k) //сложность O(n)
+        {
+            if (k <= 0) return 0;
+
+            Node p1 = head;
+            Node p2 = head;
+
+            // перемещаем p2 на k узлов вперед
+            for (int i = 0; i < k - 1; i++)
+            {
+                if (p2 == null) return 0;
+                p2 = p2.Next;
+            }
+
+            if (p2 == null) return 0;
+
+            //Теперь мы начинаем перемещать оба указателя одновременно.Когда p2 дойдет до конца списка, p1 будет указывать на нужный нам элемент
+            while (p2.Next != null)
+            {
+                p1 = p1.Next;
+                p2 = p2.Next;
+            }
+            return p1.info;
+        }
+
+        //Задание 6. Есть однонаправленный список из структур. В нем random указывает на какой-то элемент списка. Напишите функцию,
+        //которая копирует этот список с сохранением структуры. Сложность O(n),константная доп. память + память под элементы нового списка
+        public Node copyList(Node head)//cложность O(n)
+        {
+            //Делаем обход списка, создаём дубликаты узлов и вставляем их по next, получая 2*N элементов, каждый нечётный ссылается на свой дубликат.
+            for (Node cur = head; cur != null; cur = cur.Next)
+            {
+                Node dup = new Node();
+                dup.info = cur.info;
+                dup.Next = cur.Random;
+                cur.Random = dup;
+            }
+
+            Node result = head.Random;
+
+            //Делаем второй обход списка, в каждом чётном узле random = random.next.
+            for (Node cur = head; cur.Next != null; cur = cur.Next)
+            {
+                Node dup = cur.Random;
+                dup.Random = cur.Next.Random;
+            }
+
+            //Делаем третий обход списка, в каждом узле next = next.next
+            for (Node cur = head; cur != null; cur = cur.Next)
+            {
+                Node dup = cur.Random;
+                cur.Random = dup.Next;
+                dup.Next = cur.Next != null ? cur.Next.Random : null;
+            }
+            return result;
+        }
+    }
+}
---
+++ Alg_03/Alg_03/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alg_03
+{
+    class Program
+    {
+        //Задание 1. Дан однонаправленный список с петлей. Его последний элемент содержит указатель на один из элементов этого списка.
+        //Найдите начальный узел петли
+        static void FindLoop()
+        {
+            LinkList list = new LinkList();
+            Random rand = new Random();
+            for (int y = 0; y < 8; y++)
+            {
+                list.Add(rand.Next(1, 20));
+            }
+            Console.WriteLine("Список: ");
+            Console.WriteLine(list.ToString());
+            list.head.Next.Next.Next.Next.Next = list.head.Next.Next;
+            Console.WriteLine("Начало петли - узел со значением: " + list.FindBegining(list.head).info);
+            Console.WriteLine();
+        }
+        //Задание 2. Дан  список с двумя указателями. Скопируйте список за O(n) без использования доп. памяти.
+        static void Copy()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            Random rand = new Random();
+            for (int y = 0; y < 8; y++)//заполняем список
+            {
+                list.AddLast(rand.Next(1, 20));
+            }
+
+            Console.WriteLine("Начальный список: ");
+            foreach (int i in list)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+
+            LinkedList<int> copiedList = new LinkedList<int>();
+            LinkedListNode<int> p = list.First;
+
+            while (p != null)//заполняем новый список копиями из начального списка
+            {
+                copiedList.AddLast(p.Value);
+                p = p.Next;
+            }
+
+            Console.WriteLine("Скопированный список: ");
+            foreach (int i in copiedList)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine("\n");
+        }
+
+        //Задание 3. Удалите дубликаты из несортированного связного списка. Память константна
+        static void Delete()
+        {
+            LinkList list = new LinkList();
+            Random rand = new Random();
+            for (int y = 0; y < 6; y++)
+            {
+                list.Add(rand.Next(1, 20));
+            }
+            Console.WriteLine("Начальный список: \n" + list.ToString());
+            list.deleteDups2(list.head);
+            Console.WriteLine("Список без дубликатов: \n" + list.ToString());
+            Console.WriteLine();
+        }
+        //Задание 4. Разбейте связный список вокруг некоторого значения так, чтобы все меньше узлы оказались перед узлами,
+        //большими или равными этому значению
+        static void Partition()
+        {
+            LinkList list = new LinkList();
+            Random rand = new Random();
+            for (int y = 0; y < 6; y++)
+            {
+                list.Add(rand.Next(1, 20));
+            }
+            Console.WriteLine("Начальный список: \n" + list.ToString());
+            Console.Write("Относительно какого элемента отсортировать список? ");
+            int k = Int32.Parse(Console.ReadLine());
+            list.head = list.partition(list.head, k);
+            Console.WriteLine("Отсортированный список: \n" + list.ToString());
+            Console.WriteLine();
+        }
+        //Задание 5. Найдите в односвязном списке к-ый с конца элемент. Есть только операция получения следующего элемента
+        //и указатель на первый элемент. Алгоритм должен быть оптимален по времени и памяти
+        static void kElement()
+        {
+            LinkList list = new LinkList();
+            Random rand = new Random();
+            for (int y = 0; y < 8; y++)
+            {
+                list.Add(rand.Next(1, 20));
+            }
+            Console.WriteLine("Начальный список: \n" + list.ToString());
+            Console.Write("Какой элемент с конца вы хотите получить?  ");
+            int k = Int32.Parse(Console.ReadLine());
+            int p = list.findK(list.head, k);
+            Console.WriteLine("k-ый с конца элемент: " + p);
+            Console.WriteLine();
+        }
+        //Задание 6. Есть однонаправленный список из структур. В нем random указывает на какой-то элемент списка. Напишите функцию,
+        //которая копирует этот список с сохранением структуры. Сложность O(n),константная доп. память + память под элементы нового списка
+        static void RandomNode()
+        {
+            LinkList list = new LinkList();
+            Random rand = new Random();
+            for (int y = 0; y < 8; y++)
+            {
+                list.Add(rand.Next(1, 20));
+            }
+            Console.WriteLine("Начальный список: " + list.ToString());
+            list.head = list.copyList(list.head);
+            Console.WriteLine("Скопированный список: " + list.ToString());
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №3." +
+                "\n Выполнила: Андреева Анна. Группа: 6213-020302D");
+            while (true)
+            {
+                Console.WriteLine("Выберите задание: \n 0)Выход \n 1) Нахождение начала петли \n 2)Копирование двусвязного списка " +
+                    "\n 3)Удаление дубликатов \n 4)Сортировка относительно элемента \n 5)Нахождение k-того элемента " +
+                    "\n 6)Копирование списка с рандомными связями ");
+                Console.Write("Ваш выбор: ");
+                string selected = Console.ReadLine();
+                Console.WriteLine();
+                switch (selected)
+                {
+                    case "0": return;
+                    case "1": FindLoop(); break;
+                    case "2": Copy(); break;
+                    case "3": Delete(); break;
+                    case "4": Partition(); break;
+                    case "5": kElement(); break;
+                    case "6": RandomNode(); break;
+                    default: Console.WriteLine("Введено не то"); break;
+                }
+                Console.WriteLine("\r\nДля возврата в меню нажмите Enter. Для выхода нажмите 0.");
+                Console.Write("Ваш выбор: ");
+                string pnt = Console.ReadLine();
+                if (pnt == "0")
+                    return;
+                else
+                    Console.Clear();
+            }
+        }
+    }
+}
